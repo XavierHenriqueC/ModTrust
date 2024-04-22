0721501923b0e240ba3507da9dc3de1eae8338e7
XavierHenriqueC <hxc_henrique@hotmail.com>
2024-04-22 17:13:48 -0300

    Iniciado esqueleto do pagina web

--- Interface/src/App.jsx
+++ Interface/src/App.jsx
@@ -1,11 +1,59 @@
 import { useState } from 'react'
 import './App.css'
 
+import Logo from "./img/favicon_round.png"
+
 function App() {
+
+  const [navState, setNavState] = useState({network: true, devices: false, tasks: false})
+
+  //Função para navegação da sidebar
+  const handlerNav = (button) => {
+    if(button === "network") {
+      setNavState({network: true, devices: false, tasks: false})
+    }
+
+    if(button === "devices") {
+      setNavState({network: false, devices: true, tasks: false})
+    }
+
+    if(button === "tasks") {
+      setNavState({network: false, devices: false, tasks: true})
+    }
+  }
  
   return (
-    <div className='App'>
-      <h1 style={{color: "red"}}>TrustBus Gateway</h1>
+    <div className='app'>
+      <div className="header">
+        <div className="logo">
+          <img src={Logo} alt="logo datatrust" />
+        </div>
+        <p>Modtrust Gateway</p>
+        <p>MAC: awfa788556dwd</p>
+      </div>
+      <div className="body">
+        <div className="sidebar">
+          <div className="nav">
+            <div onClick={() => handlerNav("network")} className={`button ${navState.network ? "selected" : ""}`}>
+              <ion-icon name="git-network-outline"></ion-icon>
+              <p>Network</p>
+            </div>
+            <div onClick={() => handlerNav("devices")} className={`button ${navState.devices ? "selected" : ""}`}>
+              <ion-icon name="cube-outline"></ion-icon>
+              <p>Devices</p>
+            </div>
+            <div onClick={() => handlerNav("tasks")} className={`button ${navState.tasks ? "selected" : ""}`}>
+              <ion-icon name="document-attach-outline"></ion-icon>
+              <p>Tasks</p>
+            </div>
+          </div>
+
+        </div>
+        <div className="center">
+          
+        </div>
+        
+      </div>
     </div>
   )
 }
